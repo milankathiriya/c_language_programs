a762e8ce2c401be0492d2a9e63632be303e2ac03
milankathiriya <milankathiriya99@gmail.com>
2024-12-18 09:36:35 +0530

    FIRST UPLOAD

---
+++ FIRST.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+// WAP to demonstrate the format specifier in detail.
+
+main()
+{
+  char a = 'T', b = 'O', c = 'Y';
+  clrscr();
+
+  printf("%c%c%c", a, b, c);
+
+  getch();
+}
